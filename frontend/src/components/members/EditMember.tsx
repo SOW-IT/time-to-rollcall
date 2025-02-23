984538b08657c3206628a99d138e2c0e76e5671f
= <kimchankwon@gmail.com>
2025-02-23 23:49:54 +1100

    try now

--- frontend/src/components/members/EditMember.tsx
+++ frontend/src/components/members/EditMember.tsx
@@ -151,7 +151,9 @@ export default function EditMember({
                       <p className="text-sm text-gray-900">
                         Name
                         {newMember &&
-                          members?.find((m) => m.name === member.name) && (
+                          members?.some(
+                            (m) => m.name.trim() === member.name.trim()
+                          ) && (
                             <span className="ml-2 text-orange-400 text-xs">
                               Member with this name already exists!
                             </span>
@@ -309,7 +311,12 @@ export default function EditMember({
                         disabled={!member.name}
                         className="mt-4 rounded-3xl border border-transparent bg-black px-4 py-2 text-sm font-medium text-white focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed"
                         onClick={() =>
-                          newMember ? openConfirmationModal() : submit()
+                          newMember &&
+                          members?.some(
+                            (m) => m.name.trim() === member.name.trim()
+                          )
+                            ? openConfirmationModal()
+                            : submit()
                         }
                       >
                         {newMember ? "Create" : "Update"}
