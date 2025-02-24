e9a489d5dc7b892496ab84958806cdcb323dbdb1
= <kimchankwon@gmail.com>
2025-02-24 14:21:36 +1100

    active

--- frontend/src/components/SignInButton.tsx
+++ frontend/src/components/SignInButton.tsx
@@ -13,7 +13,7 @@ export default function SignInButton() {
   return (
     <button
       type="button"
-      className="rounded-full text-gray-600 bg-transparent border hover:bg-gray-200 border-gray-600 font-medium text-xs px-3 py-2"
+      className="rounded-full text-gray-600 bg-transparent border hover:bg-gray-300 active:bg-gray-300 border-gray-600 font-medium text-xs px-3 py-2"
       onClick={signInWithMicrosoft}
     >
       Sign-in
