d50ec3f2861a849956254a63c099ab38cf920549
= <kimchankwon@gmail.com>
2025-10-06 23:51:24 +1100

    rollover

--- functions/src/components/rollover.ts
+++ functions/src/components/rollover.ts
@@ -2,24 +2,54 @@ import { db } from "../config/firebase";
 import { onSchedule } from "firebase-functions/scheduler";
 
 export const rolloverUsers = onSchedule(
-  { timeZone: "Australia/Sydney", schedule: "8 22 5 10 *", timeoutSeconds: 120, region: "australia-southeast1" },
+  { timeZone: "Australia/Sydney", schedule: "0 0 1 10 *", timeoutSeconds: 120, region: "australia-southeast1" },
   async () => {
     const currentYear = new Date().getFullYear();
     const nextYear = currentYear + 1;
 
     const groups = await db.collection("groups").get();
     for (const group of groups.docs) {
-      const lastYearMembers = await db
+      console.log(group.data().name);
+      const currentYearMembers = await db
         .collection("groups")
         .doc(group.id)
         .collection("members")
-        .doc(nextYear.toString())
+        .doc(currentYear.toString())
         .collection("members")
         .get();
-      for (const lym of lastYearMembers.docs) {
+      for (const lym of currentYearMembers.docs) {
+        await db
+          .collection("groups")
+          .doc(group.id)
+          .collection("members")
+          .doc(nextYear.toString())
+          .collection("members")
+          .doc(lym.id)
+          .set(lym.data());
+      }
+    }
+  }
+);
+
+export const rolloverUsersYear = onSchedule(
+  { timeZone: "Australia/Sydney", schedule: "0 0 1 1 *", timeoutSeconds: 120, region: "australia-southeast1" },
+  async () => {
+    const currentYear = new Date().getFullYear();
+
+    const groups = await db.collection("groups").get();
+    for (const group of groups.docs) {
+      console.log(group.data().name);
+      const currentYearMembers = await db
+        .collection("groups")
+        .doc(group.id)
+        .collection("members")
+        .doc(currentYear.toString())
+        .collection("members")
+        .get();
+      for (const lym of currentYearMembers.docs) {
         const metadata = lym.data().metadata;
         const mdId = "U65Ey0liZ6OeVqSusDtN";
-        if (metadata) {
+        if (metadata && mdId in metadata) {
           if (metadata[mdId] !== "6") {
             metadata[mdId] = (Number(metadata[mdId]) + 1).toString();
           }
@@ -28,7 +58,7 @@ export const rolloverUsers = onSchedule(
           .collection("groups")
           .doc(group.id)
           .collection("members")
-          .doc(nextYear.toString())
+          .doc(currentYear.toString())
           .collection("members")
           .doc(lym.id)
           .set({ ...lym.data(), metadata });
